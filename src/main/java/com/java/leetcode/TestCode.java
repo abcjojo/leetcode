89631930a41d3a697a88e297b998cb0fb9989e23
abcjojo <497601974@qq.com>
2021-07-22 14:16:58 +0800

    算法题

--- src/main/java/com/java/leetcode/TestCode.java
+++ src/main/java/com/java/leetcode/TestCode.java
@@ -1,10 +1,26 @@
 package com.java.leetcode;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+
 public class TestCode {
 
     public static void main(String[] args) {
-        String s1 = "-----yuyanggoulanzi----";
-        String s2 = s1.replace("yuyang", "zhanghaungkai");
-        System.out.println(s2);
+
+//        String s1 = "abcdasdf\n#asdf \n 123";
+//        System.out.println(s1);
+//
+//        System.out.println("分割线---------------------------------");
+//
+//        List<String> s2 = Arrays.asList(s1.split("\n"));
+//        List<String> collect = s2.stream().filter(s -> !s.trim().startsWith("#")).collect(Collectors.toList());
+//        collect.stream().forEach(s -> System.out.println(s));
+
+
     }
 }
--- src/main/java/com/java/leetcode/common/UserTest.java
+++ src/main/java/com/java/leetcode/common/UserTest.java
@@ -1,61 +1,61 @@
-package com.java.leetcode.common;
-
-import lombok.Data;
-
-import java.util.ArrayList;
-
-@Data//省去了get、set方法，需要在pom.xml文件中导入lombok依赖
-public class UserTest {
-
-    private String username;
-    private String password;
-    private Integer age;
-    public UserTest(){
-
-    }
-
-    public UserTest(String username, String password, Integer age) {
-        this.username = username;
-        this.password = password;
-        this.age = age;
-    }
-
-
-    //public static void main(final String... args)
-    public static void main(String[] args)
-    {
-        ArrayList<UserTest> list = new ArrayList<UserTest>();
-
-        list.add(new UserTest("liubei","111",40));
-        list.add(new UserTest("zhangfei","222",30));
-        list.add(new UserTest("guanyu","333",35));
-
-
-        list.removeIf(u -> "liubei".equals(u.getUsername()));
-
-        System.out.println("list: "+list.toString());
-
-
-
-
-        System.out.println("\ntest1:");
-        list.stream().map(n->n)
-                .forEach(n-> System.out.println(n));
-
-        System.out.println("\ntest2--age:");
-        list.stream().map(n->n.getAge())
-                .forEach(n-> System.out.println(n));
-
-        System.out.println("\ntest3--age:");
-        list.stream().map(n->n.getAge())
-                .map(n->n).forEach(n-> System.out.println(n));
-
-        System.out.println("\ntest2--username:");
-        list.stream().map(n->n.getUsername())
-                .forEach(n-> System.out.println(n));
-
-        System.out.println("\ntest3--username:");
-        list.stream().map(n->n.getUsername())
-                .map(n->n).forEach(n-> System.out.println(n));
-    }
-}
+//package com.java.leetcode.common;
+//
+//import lombok.Data;
+//
+//import java.util.ArrayList;
+//
+//@Data//省去了get、set方法，需要在pom.xml文件中导入lombok依赖
+//public class UserTest {
+//
+//    private String username;
+//    private String password;
+//    private Integer age;
+//    public UserTest(){
+//
+//    }
+//
+//    public UserTest(String username, String password, Integer age) {
+//        this.username = username;
+//        this.password = password;
+//        this.age = age;
+//    }
+//
+//
+//    //public static void main(final String... args)
+//    public static void main(String[] args)
+//    {
+//        ArrayList<UserTest> list = new ArrayList<UserTest>();
+//
+//        list.add(new UserTest("liubei","111",40));
+//        list.add(new UserTest("zhangfei","222",30));
+//        list.add(new UserTest("guanyu","333",35));
+//
+//
+//        list.removeIf(u -> "liubei".equals(u.getUsername()));
+//
+//        System.out.println("list: "+list.toString());
+//
+//
+//
+//
+//        System.out.println("\ntest1:");
+//        list.stream().map(n->n)
+//                .forEach(n-> System.out.println(n));
+//
+//        System.out.println("\ntest2--age:");
+//        list.stream().map(n->n.getAge())
+//                .forEach(n-> System.out.println(n));
+//
+//        System.out.println("\ntest3--age:");
+//        list.stream().map(n->n.getAge())
+//                .map(n->n).forEach(n-> System.out.println(n));
+//
+//        System.out.println("\ntest2--username:");
+//        list.stream().map(n->n.getUsername())
+//                .forEach(n-> System.out.println(n));
+//
+//        System.out.println("\ntest3--username:");
+//        list.stream().map(n->n.getUsername())
+//                .map(n->n).forEach(n-> System.out.println(n));
+//    }
+//}
